4c46ef6fc4f108b121dbb254cd96034e466d93f7
Emile Nijssen <emile@emilenijssen.nl>
2023-09-29 20:53:41 +0200

    v1.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -10,8 +10,8 @@ android {
         applicationId = "net.weejewel.usbaudioinputplayer"
         minSdk = 28
         targetSdk = 33
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 2
+        versionName = "1.1"
 
     }
 
